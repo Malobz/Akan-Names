b90122114255df58b9b671ccf28ce1d8c9676115
mercy maloba <your_email_address@example.com>
2021-06-16 01:01:37 +0300

    delete main.js

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-function userInput(){
-    var date = prompt( "Enter the day you were born");
-    var month = prompt (" Enter the month you were born");
-    var year = prompt("Enter theyear you were born");
-    }
-    userInput()
-
